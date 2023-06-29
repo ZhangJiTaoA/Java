e10009a85c706169b22c295e8ca524f698ae7baf
zjt <597778912@qq.com>
2023-06-29 22:10:34 +0800

    提交两个

---
+++ src/main/java/org/study/e_commonSense.java
@@ -0,0 +1,24 @@
+package org.study;
+/*
+    java中包管理机制是管理类的重要手段
+    相当于文件夹对文件的关系
+    package: 名称.域名的倒写
+
+    写项目都要加包
+    java中常用的包：
+        1. java.lang   // 核心包(系统带的，无需导入，直接使用)
+        2. java.awt    // 画窗口
+        3. java.net    // 网络编程
+        4. java.io     // IO流
+        5. java.util   // 工具类
+
+    导入类 import
+        如果使用多包下的多个类，可使用“*”（会降低编译速度，但是不会影响运行速度）
+        例如  System.util.*
+        最好还是精确写吧。
+    静态导入：
+        import static java.lang.Math.* // 将Math类中所有静态属性导入
+ */
+import static java.lang.Math.*;
+public class e_commonSense {
+}
